fa8682ffc8b5d0121ef39c0eff64327b7026164d
Yifan(Maggie) Hou <63317173+yexiu52937@users.noreply.github.com>
2023-08-11 18:05:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 
   /* Connect to MySQL and select the database. */
 
-  echo DB_SERVER . DB_USERNAME . DB_PASSWORD . DB_DATABASE;
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
   if (mysqli_connect_error()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -14,7 +13,6 @@
   $getFilmTable = "SELECT * FROM Film";
 
   $results = mysqli_query($connection, $getFilmTable);
-  echo "getting results";
 
   if ($results === false) {
     echo mysqli_error($connection);
@@ -22,5 +20,4 @@
     $Films = mysqli_fetch_all($results);
     var_dump($Films);
   }
-  echo "finish";
 ?>
