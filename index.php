a46b7d9ece79c9a33c7c0c23cdce541335212d04
Yifan(Maggie) Hou <63317173+yexiu52937@users.noreply.github.com>
2023-08-11 17:26:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
+<?php include "../inc/dbinfo.inc";
 
-echo "test";
+  /* Connect to MySQL and select the database. */
+  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
+  if (mysqli_connect_error()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+  $getFilmTable = "SELECT * FROM Film";
+
+  $results = mysqli_query($connection, $getFilmTable);
+
+  if ($results === false) {
+    echo mysqli_error($connection);
+  } else {
+    $Films = mysqli_fetch_all($results);
+    var_dump($Films);
+  }
 ?>
