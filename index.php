85028d29c37167a703b5271d7c3fffbefc98a1b9
Yifan Hou <y7hou@ucsd.edu>
2023-08-12 22:39:41 -0700

    change index.php and add search.php

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<?php
-  
-  include "../inc/dbinfo.inc";
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Movie Manager</title>
+    <meta charset="utf-8">
+  </head>
 
-  /* Connect to MySQL and select the database. */
-
-  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-  if (mysqli_connect_error()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
-  // $database = mysqli_select_db($connection, DB_DATABASE);
-
-  $getFilmTable = "SELECT * FROM Film";
-
-  $results = mysqli_query($connection, $getFilmTable);
-
-  if ($results === false) {
-    echo mysqli_error($connection);
-  } else {
-    $Films = mysqli_fetch_all($results);
-    var_dump($Films);
-  }
-?>
+  <body>
+    <form action="search.php" method="GET">
+      <input type="text" name="title_query"/>
+      <input type="submit" value="Search"/>
+    </form>
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,39 @@
+<?php
+  
+    include "../inc/dbinfo.inc";
+
+    /* Connect to MySQL and select the database. */
+
+    $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+    if (mysqli_connect_error()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+    // $database = mysqli_select_db($connection, DB_DATABASE);
+
+//   $getFilmTable = "SELECT * FROM Film";
+
+//   $results = mysqli_query($connection, $getFilmTable);
+
+//   if ($results === false) {
+//     echo mysqli_error($connection);
+//   } else {
+//     $Films = mysqli_fetch_all($results);
+//     var_dump($Films);
+//   }
+
+    $query = $_GET['title_query'];
+    $searchInTitle = "SELECT * FROM Film WHERE titolo LIKE '%".$query."%';";
+    $raw_results = mysqli_query($connection, $searchInTitle);
+    if ($raw_results === false) {
+        echo mysqli_error($connection);
+    } else {
+        if(mysqli_num_rows($raw_results) > 0){
+			while($results = mysqli_fetch_array($raw_results)){
+				echo $results;
+			}
+		}
+		else{
+			echo "No results";
+		}
+    }
+?>
