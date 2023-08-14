6617add428ae4706dec9f9f9580b37c5eb41ec1d
Yifan(Maggie) Hou <63317173+yexiu52937@users.noreply.github.com>
2023-08-14 11:42:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>Movie Manager</title>
-    <link rel="stylesheet" href="style.css"></link>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <meta charset="utf-8">
   </head>
 
