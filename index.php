ad456ae8072545e427f3cf64ca3a5e8e4a4f2eb3
Yifan(Maggie) Hou <63317173+yexiu52937@users.noreply.github.com>
2023-08-14 11:53:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
   </head>
 
   <body>
-    <form action="search.php" method="GET">
-      <input type="text" class="form-control" placeholder="Movie title" name="title_query"/>
-      <input type="submit" class="form-control" value="Search"/>
+    <form action="search.php" method="GET" class="row row-cols-lg-auto g-3 align-items-center">
+      <div class="col-12">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Movie title" name="title_query"/>
+        </div>
+      </div>
+
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary">Search</button>
+      </div>
     </form>
   </body>
 </html>
