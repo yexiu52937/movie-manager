3855b47095d74903c80c01584714d155b5ce0ac9
Yifan(Maggie) Hou <63317173+yexiu52937@users.noreply.github.com>
2023-08-14 13:18:10 +0800

    Update search.php

--- search.php
+++ search.php
@@ -29,7 +29,7 @@
 
   <body>
 
-    <div class="d-flex justify-content-center">
+    <div class="mt-1 mx-auto">
       <form class="row row-cols-lg-auto g-3 align-items-center">
         <div class="col-12">
           <div class="input-group">
